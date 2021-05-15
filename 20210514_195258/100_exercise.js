a32e2a90d0ca05d9ea1d73812930e6fda043578e
Tin Tran <bkvtin@gmail.com>
2021-05-15 12:50:11 +0700

    20210514_195258 update exercise

--- 20210514_195258/100_exercise.js
+++ 20210514_195258/100_exercise.js
@@ -1,52 +1,56 @@
-/* ------------------------------------------
-   load thư viện
-   ------------------------------------------*/
 const fs = require('fs');
 
-/* ------------------------------------------
-   tạo folder
-   ------------------------------------------*/
-
-function create_folder(folder_name) {
-    return new Promise((resolve, reject) => {
-        fs.mkdir(folder_name, (error) => {
-            if (error) throw error;
-            console.log(`mkdir: created folder ${folder_name}`)
-            resolve('mkdir: okay');
-
-        });
-        reject('mkdir: error');
-    })
-}
-
-function create_file(file_name) {
-    return new Promise((resolve, reject) => {
-        fs.open(file_name, 'w', (error, file) => {
-            if (error) throw error;
-        });
-        reject('mkdir: error');
-    })
-}
-
-create_folder('./project');
-
-var folder_arrs = ['./project/controller', './project/models', './project/node_modules', './project/public', './project/view']
-var file_controllers_arrs = ['app_class.js', 'category_api.js']
-
-folder_arrs.forEach(function create_folder(folder_name) {
-    fs.mkdir(folder_name, (error) => {
-        if (error) throw error;
-
-        if (folder_name.includes('controller')) {
-            file_controllers_arrs.forEach(function create_file(file_name) {
-                fs.open(`${folder_name}\${file_name}`, 'w', (error, file) => {
-                    if (error) throw error;
-                });
-                console.log(`touch: created file ${file_name}`)
-            });
-            
-        }
-        console.log(`mkdir: created folder ${folder_name}`)
+function create_dir(dir_path) {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(dir_path, (error) => {
+      if (error) {
+        console.error("[ERROR] create_dir: ", error);
+        reject("[ERROR] create_dir: ", error);
+      } else {
+        console.log("[INFO] create_dir is okay: ", dir_path);
+        resolve("[INFO] create_dir is okay: ", dir_path);
+      }
     });
-});
+  });
+}
 
+function create_file(file_path) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file_path, '', (error) => {
+      if (error) {
+        console.error("[ERROR] create_file: ", error);
+        reject("[ERROR] create_file: ", error);
+      } else {
+        console.log("[INFO] create_file is okay: ", file_path);
+        resolve("[INFO] create_file is okay: ", file_path);      
+      }
+    });
+  });
+}
+
+async function main() {
+  var parent_path = './project';
+  var folder_arr = ['controller', 'models', 'node_modules', 'public', 'view'];
+  var folder_chil_arr = ['view/category', 'view/partials', 'view/products', 'view/users']
+
+  var file_arr = [
+    'controller/app_class.js', 'controller/category_api.js', 
+    'view/index.ejs', 'view/partials/aside.ejs', 'view/partials/css.ejs', 'view/partials/footer.ejs', 'view/partials/header.ejs', 'view/partials/js.ejs', 'view/partials/main_home.ejs', 'view/partials/nav.ejs'
+  ];
+
+  // create parent dir
+  await create_dir(parent_path);
+
+  // create child/sub dirs
+  const f_ps = folder_arr.map(n => create_dir(`${parent_path}/${n}`));
+  await Promise.all(f_ps);
+
+  const f_chil_ps = folder_chil_arr.map(n => create_dir(`${parent_path}/${n}`));
+  await Promise.all(f_chil_ps);
+
+  // create file in child/sub dir
+  const file_ps = file_arr.map(n => create_file(`${parent_path}/${n}`));
+  await Promise.all(file_ps);
+}
+
+main();
