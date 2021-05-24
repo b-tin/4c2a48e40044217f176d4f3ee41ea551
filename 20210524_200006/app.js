0c82ada8a0dae47d7ef32e6db2d18a98f4c42d08
Tin Tran <tin.tran@geniebook.com>
2021-05-24 21:35:06 +0700

    [20210524_200006] router with mvc (model-view-controller)

---
+++ 20210524_200006/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+/**
+ * gọi ejs
+ */
+app.set('view engine', 'ejs');
+
+// 1. <%- name %>                   : render html
+// 2. <%= name %>                   : render text
+// 3. <%- include('path') %>        : gọi đến file
+// 4. <% if, for, forEach, ... %>
+
+
+/**
+ * gọi đên control_controller
+ */
+const control_controller = require('./controllers/control_controller');
+app.use('/', control_controller);
+
+
+app.listen(3000, () => {
+    console.log('[INFO] server listening on port 3000');
+});
